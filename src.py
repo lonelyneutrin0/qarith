a76673d030fed9716e384771dc79b680301f55fc
Pixelqted <pixelatedwastaken@gmail.com>
2024-12-28 15:19:07 -0500

    Implemented basic quantum arithmetic

--- src.py
+++ src.py
@@ -1,8 +1,10 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 import qiskit
 import numpy as np 
+import qiskit.circuit
 from qiskit_aer import Aer
 from typing import Tuple 
+
 #############################################################
 #                 Quantum Fourier Transform                 #
 #############################################################
@@ -127,12 +129,14 @@ def iQFT(self: QuantumCircuit,
         qft_circ = QFTCircuit(n)
         invqft_circ = qft_circ.inverse()
         invqft_circ.decompose() 
+        invqft_circ.name='iQFT'
         self.append(invqft_circ, self.qubits[:n])
     elif target_range is None: 
         n = target.size
         qft_circ = QFTCircuit(n)
         invqft_circ = qft_circ.inverse()
         invqft_circ.decompose() 
+        invqft_circ.name='iQFT'
         self.append(invqft_circ, target)
     
     else: 
@@ -140,112 +144,145 @@ def iQFT(self: QuantumCircuit,
         qft_circ = QFTCircuit(n)
         invqft_circ = qft_circ.inverse()
         invqft_circ.decompose() 
+        invqft_circ.name='iQFT'
         self.append(invqft_circ, qargs=np.arange(start=target_range[0], stop=target_range[1]).tolist())        
 
 QuantumCircuit.QFT = QFT
 QuantumCircuit.iQFT = iQFT
 
 #############################################################
-#                 Quantum Arithmetic Gates                  #
+#                  Custom Controlled Gates                  #
 #############################################################
 
-def FullCarryAdder(
-        A: QuantumRegister, 
-        B: QuantumRegister, 
-        carry_in: QuantumRegister=None, 
-        carry_out: QuantumRegister=None, 
-        S: QuantumRegister=None,
-        classical_S: ClassicalRegister=None, 
-        classical_carry_out: ClassicalRegister=None
-        )->qiskit.QuantumCircuit:
-
+def ccp(
+    self, 
+    theta: float, 
+    control_qubit1, 
+    control_qubit2, 
+    target_qubit
+    )->None: 
+    
     """
-    N-Qubit Adder Circuit using Toffoli and CNOT gates. (2N+1, 2N) Implementation. Use .compose() to attach to other circuits
-    :param A: Input Register 1 (N Qubits)
-    :param B: Input Register 2 (N Qubits)
-    :param carry_in: carry-in register (1 qubit only)
-    :param carry_out: carry-out register (N Qubits)
-    :param S: Partial Sum Register (N Qubits)
-    :param classical_S: Partial Sum Storage (N Bits)
-    :param classical_carry_out: Carry-out Register (N bits only)
-    :returns Instruction: If all registers are specified correctly 
-    :returns Circuit: If one or more required Registers are not specified
+    Implementation Specifications
+    ------------------------------
+    Controlled controlled phase gate (CCRZ/CCP)
+    
+    Qubit Requirement - 3
+    
+    Introduces a phase change in target qubit if both control qubits are true
+    
+    Parameters
+    ----------- 
+    
+    self : QuantumCircuit 
+        The circuit to which the gate is to be added. 
+    
+    theta : float 0 <= θ <= 2π
+        The phase change to introduce
+    
+    control_qubit1 : QubitSpecifier
+        The first control qubit 
+    
+    control_qubit2 : QubitSpecifier
+        The second control qubit 
+    
+    target_qubit : QubitSpecifier
+        The target qubit
+    
+    Return Object
+    ---------------
+    
+    None : This is purely a mutator method 
     """
-    if(all(x is not None for x in [carry_in, carry_out, S, classical_carry_out, classical_S])): 
-        N = A.size
-        for i in [B, carry_out, S, classical_carry_out, classical_S]: 
-            if i.size != N: raise ValueError("Improper Register Qubit Count!")
-        if(carry_in.size != 1): raise ValueError("Improper Carry-In Qubit")
-        
-        circuit = QuantumCircuit(A,B,S,carry_in, carry_out, classical_S, classical_carry_out)
-        
-        for i in range(N): 
-            
-            # Compute the partial sum
-            circuit.cx(A[i], S[i])
-            circuit.cx(B[i], S[i])
-            if i == 0: 
-                circuit.cx(carry_in[0], S[i])
-            else: 
-                circuit.cx(carry_out[i-1], S[i])
-            
-            # Carry Out Calculations
-            if i == 0: 
-                circuit.ccx(A[i], B[i], carry_out[i])
-                circuit.ccx(A[i], carry_in[0], carry_out[0])
-                circuit.ccx(B[i], carry_in[0], carry_out[0])
-            else: 
-                circuit.ccx(A[i], B[i], carry_out[i])
-                circuit.ccx(A[i], carry_out[i-1], carry_out[i])
-                circuit.ccx(B[i], carry_out[i-1], carry_out[i])
-        
-        # Measurements 
-        for i in range(N): 
-            circuit.measure(S[i], classical_S[i])
-            circuit.measure(carry_out[i], classical_carry_out[i])
-        
-        instr = circuit.to_instruction()
-        instr.name = "Adder"
-        return instr
-    else: 
-        if A.size != B.size: raise ValueError("The given quantum registers are not of the same size")
-        if carry_in is None: 
-            carry_in = QuantumRegister(1)
-        N = A.size
-        
-        S = QuantumRegister(N, 'sum')
-        carry_out = QuantumRegister(N, 'c_out')
-        c_S = ClassicalRegister(N, 'c_sum')
-        c_carry_out = ClassicalRegister(N, 'c_c_out')
+    rz_matrix = np.array([[1, 0],
+                        [0, np.exp(1j * theta)]])
+    
+    rz_matrix = np.block(
+        [
+        [np.eye(2), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2))], 
+        [np.zeros((2,2)), np.eye(2), np.zeros((2,2)), np.zeros((2,2))], 
+        [np.zeros((2,2)), np.zeros((2,2)), np.eye(2), np.zeros((2,2))],
+        [np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), rz_matrix]
+        ]
+        )
+    
+    ccp = qiskit.circuit.library.UnitaryGate(rz_matrix, label='ccp')
+    if control_qubit1 == control_qubit2: 
+        self.cp(theta, control_qubit1, target_qubit)
+    else:
+        self.append(ccp, [control_qubit1, control_qubit2, target_qubit])
 
-        circuit = QuantumCircuit(A,B,S,carry_in, carry_out, c_S, c_carry_out)
-        
-        for i in range(N): 
+def cccp(self, theta, control_qubit1, control_qubit2, control_qubit3, target_qubit):    
+    """
+    Implementation Specifications
+    ------------------------------
+    Controlled controlled controlled phase gate (CCCRZ/CCCP)
+    
+    Qubit Requirement - 3
+    
+    Introduces a phase change in target qubit if all control qubits are true
+    
+    Parameters
+    ----------- 
+    
+    self : QuantumCircuit 
+        The circuit to which the gate is to be added. 
+    
+    theta : float 0 <= θ <= 2π
+        The phase change to introduce
+    
+    control_qubit1 : QubitSpecifier
+        The first control qubit 
+    
+    control_qubit2 : QubitSpecifier
+        The second control qubit 
+    
+    control_qubit3 : QubitSpecifier
+        The third control qubit 
 
-            # Compute the partial sum
-            circuit.cx(A[i], S[i])
-            circuit.cx(B[i], S[i])
-            if i == 0: 
-                circuit.cx(carry_in[0], S[i])
-            else: 
-                circuit.cx(carry_out[i-1], S[i])
-            
-            # Carry Out Calculations
-            if i == 0: 
-                circuit.ccx(A[i], B[i], carry_out[i])
-                circuit.ccx(A[i], carry_in[0], carry_out[0])
-                circuit.ccx(B[i], carry_in[0], carry_out[0])
-            else: 
-                circuit.ccx(A[i], B[i], carry_out[i])
-                circuit.ccx(A[i], carry_out[i-1], carry_out[i])
-                circuit.ccx(B[i], carry_out[i-1], carry_out[i])
+    target_qubit : QubitSpecifier
+        The target qubit
+    
+    Return Object
+    ---------------
+    
+    None : This is purely a mutator method 
+    """
+    rz_matrix = np.array([
+                [1, 0],
+                [0, np.exp(1j * theta)]
+                ])
+    
+    rz_matrix = np.block([
+        [np.eye(2), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2))], 
+        [np.zeros((2,2)), np.eye(2), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2))],
+        [np.zeros((2,2)), np.zeros((2,2)), np.eye(2), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2))],
+        [np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.eye(2), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2))], 
+        [np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.eye(2), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2))], 
+        [np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.eye(2), np.zeros((2,2)), np.zeros((2,2))],
+        [np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.eye(2), np.zeros((2,2))],
+        [np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), np.zeros((2,2)), rz_matrix] 
+    ])
 
-        # Measurements 
-        for i in range(N): 
-            circuit.measure(S[i], c_S[i])
-            circuit.measure(carry_out[i], c_carry_out[i])
-        
-        return circuit
+    cccp = qiskit.circuit.library.UnitaryGate(rz_matrix, label='ccp')
+    if control_qubit1 == control_qubit2 == control_qubit3: 
+        self.cp(theta, control_qubit1, target_qubit)
+    elif control_qubit1 == control_qubit2: 
+        self.ccp(theta, control_qubit1, control_qubit3, target_qubit)
+    elif control_qubit2 == control_qubit3: 
+        self.ccp(theta, control_qubit1, control_qubit2, target_qubit)
+    elif control_qubit1 == control_qubit3: 
+        self.ccp(theta, control_qubit2, control_qubit3, target_qubit)
+    else:
+        self.append(cccp, [control_qubit1, control_qubit2, control_qubit3, target_qubit])
+
+
+QuantumCircuit.ccp = ccp
+QuantumCircuit.cccp = cccp 
+
+#############################################################
+#                 Quantum Arithmetic Gates                  #
+#############################################################
 
 def Adder(self: QuantumCircuit, A: QuantumRegister, B: QuantumRegister, sum: QuantumRegister=None)->None: 
     """ 
@@ -260,9 +297,12 @@ def Adder(self: QuantumCircuit, A: QuantumRegister, B: QuantumRegister, sum: Qua
     Hence, the function actually operates on a qudit as |A⟩|B⟩ ↦ |A⟩|A+B mod 2^d⟩.
     This is resolved by represented the qudits in d+1 qubits (e.g. 011 -> 0011)
     
+    This implementation uses an AQFT with a k-cutoff of log2(n), which is more 
+    accurate than QFT in the presence of decoherence [Draper].
+    
     Parameters
     ----------- 
-
+    
     self : QuantumCircuit 
         The circuit to which the gate is to be added. 
     
@@ -275,10 +315,10 @@ def Adder(self: QuantumCircuit, A: QuantumRegister, B: QuantumRegister, sum: Qua
     sum : QuantumRegister, size d [see qubit requirements above], optional 
         The output register for the sum. 
         Defaults to None, output is mapped to B
-
+    
     Return Object
     ---------------
-
+    
     None : This is purely a mutator method 
     """
     
@@ -294,10 +334,11 @@ def Adder(self: QuantumCircuit, A: QuantumRegister, B: QuantumRegister, sum: Qua
             for j in range(B.size-i-1): 
                 self.cp(np.pi/2**(B.size-i-1-j), B[j], B[B.size-i-1])
         
-        # QFT from A to B
+        # CP from A to B
         for i in range(B.size): 
             for j in range(B.size-i-1): 
-                self.cp(np.pi/2**(B.size-i-1-j), A[j], B[B.size-i-1])   
+                if B.size-i-j-1 < np.log2(B.size):
+                    self.cp(np.pi/2**(B.size-i-1-j), A[j], B[B.size-i-1])   
             
         # Phase Shifts from A 
         for i in range(B.size): 
@@ -323,5 +364,150 @@ def Adder(self: QuantumCircuit, A: QuantumRegister, B: QuantumRegister, sum: Qua
             self.swap(sum[i], sum[sum.size-i-1])   
         
         self.iQFT(sum)
+
+def Subtractor(self, A: QuantumRegister, B: QuantumRegister, difference: QuantumRegister=None)->None:
+    """ 
+    Implementation Specifications
+    ------------------------------
+    Quantum Fourier Transform based subtractor gate. |A⟩|B⟩ ↦ |A⟩|A-B⟩ OR |A⟩|B⟩|0⟩ ↦ |A⟩|B⟩|A-B⟩
+    Negative numbers wrap around 
+    
+    Qubit Requirement - (2N+2) for 2 N-Qubit Integers
+    
+    Strictly speaking, only 2N Qubits are needed for the gate.
+    However, QFT-based subtraction is intrinsically modular. 
+    Hence, the function actually operates on a qudit as |A⟩|B⟩ ↦ |A⟩|A-B mod 2^d⟩.
+    This is resolved by represented the qudits in d+1 qubits (e.g. 011 -> 0011)
+    
+    Parameters
+    ----------- 
+    
+    self : QuantumCircuit 
+        The circuit to which the gate is to be added. 
+    
+    A : QuantumRegister, size d [see qubit requirements above]
+        The first input register for the difference 
+    
+    B : QuantumRegister, size d [see qubit requirements above]
+        The second input register for the difference
+    
+    difference : QuantumRegister, size d [see qubit requirements above], optional 
+        The output register for the difference. 
+        Defaults to None, output is mapped to B
+    
+    Return Object
+    ---------------
+    
+    None : This is purely a mutator method 
+    """
+    
+    # Check gate sizes 
+    if A.size != B.size: raise ValueError("Improper register size!")
+    
+    # If a designated difference register is not specified
+    if(difference is None): 
         
-QuantumCircuit.Adder = Adder
+        # QFT on B 
+        for i in range(B.size): 
+            self.h(B[B.size-i-1])
+            for j in range(B.size-i-1): 
+                self.cp(-np.pi/2**(B.size-i-1-j), B[j], B[B.size-i-1])
+        
+        # QFT from A to B
+        for i in range(B.size): 
+            for j in range(B.size-i-1): 
+                self.cp(np.pi/2**(B.size-i-1-j), A[j], B[B.size-i-1])   
+            
+        # Phase Shifts from A 
+        for i in range(B.size): 
+            self.cp(np.pi, A[i], B[i])
+        for i in range(B.size//2): 
+            self.swap(B[i], B[B.size-i-1])          
+        
+        self.iQFT(B)
+    
+    else: 
+        if difference.size != A.size: raise ValueError("Improper registry size!")
+        for i in range(difference.size): 
+            self.h(difference[difference.size-i-1])
+            for j in range(difference.size-i-1): 
+                self.cp(np.pi/2**(difference.size-i-1-j), A[j], difference[difference.size-i-1])
+        for i in range(difference.size): 
+            for j in range(difference.size-i-1): 
+                self.cp(-np.pi/2**(difference.size-i-1-j), B[j], difference[difference.size-i-1])   
+        for i in range(difference.size): 
+            self.cp(np.pi, A[i], difference[i])
+            self.cp(-np.pi, B[i], difference[i])
+        for i in range(difference.size//2): 
+            self.swap(difference[i], difference[difference.size-i-1])   
+        
+        self.iQFT(difference)
+
+def Multiplier(self, A: QuantumRegister, B: QuantumRegister, prod: QuantumRegister)->None: 
+    """ 
+    Implementation Specifications
+    ------------------------------
+    Quantum Fourier Transform based multiplier gate. |A⟩|B⟩|0⟩ ↦ |A⟩|B⟩|A*B⟩
+    
+    Qubit Requirement - (6N) for 2 N-Qubit Integers (worst case)
+    
+    Strictly speaking, only 3N Qubits are needed for the gate.
+    However, QFT-based multiplication is intrinsically modular. 
+    Hence, the function actually operates on a qudit as |A⟩|B⟩|0⟩ ↦ |A⟩|B⟩|A*B mod 2^d⟩.
+    This is resolved by represented the qudits in 2d qubits (e.g. 011 -> 000011)
+    
+    Parameters
+    ----------- 
+    
+    self : QuantumCircuit 
+        The circuit to which the gate is to be added. 
+    
+    A : QuantumRegister, size d [see qubit requirements above]
+        The first input register for the difference 
+    
+    B : QuantumRegister, size d [see qubit requirements above]
+        The second input register for the difference
+    
+    prod : QuantumRegister, size d [see qubit requirements above], optional 
+        The output register for the difference. 
+        Defaults to None, output is mapped to B
+    
+    Return Object
+    ---------------
+    
+    None : This is purely a mutator method 
+    """
+    n = A.size 
+    if(B.size != n or prod.size != n): raise ValueError("Improper register size!")
+    
+    for i in range(prod.size): 
+        self.h(prod[i])
+        
+    for k in range(prod.size): 
+        for i in range(prod.size): 
+            for j in range(B.size-i-1): 
+                self.ccp((2**k)*np.pi/2**(B.size-i-1-j), A[j], B[k], prod[B.size-i-1])  
+    
+    for k in range(prod.size): 
+        for i in range(prod.size): 
+            self.ccp((2**k)*np.pi, A[i], B[k], prod[i])
+    
+    for i in range(prod.size//2): 
+        self.swap(prod[i], prod[B.size-i-1])
+    
+    self.iQFT(prod)
+
+QuantumCircuit.Multiplier = Multiplier
+QuantumCircuit.Subtractor = Subtractor
+QuantumCircuit.Adder = Adder 
+
+#############################################################
+#                     Utility Functions                     #
+#############################################################
+
+def encode(self, reg: QuantumRegister, n: int ): 
+    for i in range(reg.size): 
+        if (n >> i) & 1: 
+            self.x(reg[i])
+
+QuantumCircuit.encode = encode
